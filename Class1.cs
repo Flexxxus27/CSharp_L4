3f7e35941413d5092d059c46a50fa249f3ecc0e4
Алексей <berezin2001@bk.ru>
2024-05-13 22:16:51 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_L4
+{
+    public abstract class Geometric
+    {
+        public abstract double S();
+        public abstract double P();
+
+    }
+
+    public class Triangle : Geometric
+    {
+        public int A{get; set;}
+        public int B { get; set;}
+        public int C { get; set;}
+
+         public Triangle(int A, int B, int C) 
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }   
+        public override double S() 
+        {
+            return A * B * C;
+        }
+        public override double P()
+        {
+            return A + B + C;
+        }
+    }
+
+    public class Squere : Geometric
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+        public int D { get; set; }
+
+        public Squere(int A, int B, int C, int D)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+            this.D = D;
+        }
+        public override double S()
+        {
+            return A * B * C * D;
+        }
+        public override double P()
+        {
+            return A + B + C + D;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharp_L4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public abstract class CDA
+    {
+        public abstract void B(int x);
+        public virtual void A() { }
+        public void asd(int x) { }
+    }
+
+
+    public class ABC 
+    { 
+        public ABC() { }
+        public virtual void A() { }
+        public abstract void B(int x);
+    }
+
+    public class BCA : ABC
+    {
+        public BCA() : base() { }
+    }
+
+}
